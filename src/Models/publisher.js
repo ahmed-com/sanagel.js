740e20d680e62205e115052bcecbda1e49221dcd
Ahmed Grwan <ahmed0grwan@gamil.com>
2020-03-18 02:26:25 +0200

    finished the second draft of the model

--- src/Models/publisher.js
+++ src/Models/publisher.js
@@ -5,14 +5,11 @@ const moment = require('moment');
 
 exports.get = nameSpace =>{
 
-    const nameSpaceERC = `${nameSpace}ERC`;
+    const nameSpaceRRCs = `${nameSpace}RRCs`;
     const nameSpaceERCs = `${nameSpace}ERCs`;
-    const nameSpaceESCs = `${nameSpace}ESCs`;
-    const nameSpaceESC = `${nameSpace}ESC`;
+    const nameSpaceESCs = `${nameSpace}ESCs`;    
     const nameSpaceRSCs = `${nameSpace}RSCs`;
-    const nameSpaceRSC = `${nameSpace}RSC`;
     const nameSpaceUsers = `${nameSpace}Users`;
-    const nameSpaceUserId = `${nameSpace}UserId`;
 
     const IO = io.of(`/${nameSpace}`);
 
@@ -23,10 +20,9 @@ exports.get = nameSpace =>{
 
         subscribe(userId){  
             const room = this.id;
-            const query = "INSERT INTO :nameSpaceRSCs (`id`,`room`,`createdAt`,`updatedAt`,:nameSpaceUserId) VALUES (DEFAULT,:room,:now,:now,:userId);"             
+            const query = "INSERT INTO :nameSpaceRSCs(`room`,`createdAt`,`updatedAt`,`user`) VALUES (:room,:now,:now,:userId);"
             return pool.myExecute(query,{
                 nameSpaceRSCs,
-                nameSpaceUserId,
                 room,
                 userId,
                 now : moment(Date.now()).format('YYYY-MM-DD HH:mm:ss')
@@ -35,10 +31,9 @@ exports.get = nameSpace =>{
         
         unsubscribe(userId){      
             const room = this.id;  
-            const query = "DELETE FROM `:nameSpaceRSCs` WHERE `:nameSpaceUserId` = :userId AND `room` = :room"
+            const query = "DELETE FROM `:nameSpaceRSCs` WHERE `user` = `:userId` AND `room` = :room ;"
             return pool.myExecute(query,{
                 nameSpaceRSCs,
-                nameSpaceUserId,
                 userId,
                 room
             });
@@ -46,17 +41,16 @@ exports.get = nameSpace =>{
 
         getSubscribers(){
             const room = this.id;
-            const query = 'SELECT `:nameSpaceUsers`.* , `:nameSpaceRSCs`.`id` AS `subscriptionId`, `:nameSpaceRSCs`.`room` AS `room`, `:nameSpaceRSCs`.`createdAt` AS `subscriptionDate` FROM `:nameSpaceUsers` AS `:nameSpaceUsers` INNER JOIN `:nameSpaceRSCs` AS `:nameSpaceRSCs` ON `:nameSpaceUsers`.`id` = `:nameSpaceRSCs`.`:nameSpaceUserId` AND `:nameSpaceRSCs`.`room` = :room ;';
+            const query = 'SELECT `:nameSpaceUsers`.* , `:nameSpaceRSCs`.`room` AS `room`, `:nameSpaceRSCs`.`createdAt` AS `subscriptionDate` FROM `:nameSpaceUsers` INNER JOIN `:nameSpaceRSCs` ON `:nameSpaceUsers`.`id` = `:nameSpaceRSCs`.`:user` AND `:nameSpaceRSCs`.`room` = :room ;';
             return pool.myExecute(query,{
                 room,                
                 nameSpaceRSCs,
-                nameSpaceUsers,
-                nameSpaceUserId                
+                nameSpaceUsers,         
             });
         }        
 
         static getUser(userId){
-            const query = "SELECT `id`, `userName`, `mail`, `createdAt`, `updatedAt` FROM `:nameSpaceUsers` AS `users` WHERE `users`.`id` = :userId;";
+            const query = "SELECT `id`, `userName`, `mail`, `createdAt`, `updatedAt` FROM `:nameSpaceUsers` WHERE `id` = `:userId` LIMIT 1;";
             return pool.myExecute(query,{
                 nameSpaceUsers,
                 userId
@@ -87,34 +81,31 @@ exports.get = nameSpace =>{
             IO.connected[socketId].join(this.id);
         }
 
-        static getRooms(userId){
-            const query = "SELECT `id`, `room`, `createdAt`, `updatedAt`, :nameSpaceUserId FROM :nameSpaceRSCs AS :nameSpaceRSC WHERE :nameSpaceRSC.:nameSpaceUserId = :userId;";
+        static getRoomsByUser(userId){
+            const query = "SELECT `:nameSpaceRRCs`.* , `:nameSpaceRSCs`.`createdAt` AS `subscriptionDate`, `:nameSpaceRSCs`.`accessLevel` AS `accessLevel` FROM :nameSpaceRRCs INNER JOIN `:nameSpaceRSCs` ON `:nameSpaceRRCs`.`id` = `:nameSpaceRSCs`.`room` AND `:nameSpaceRSCs`.`user` = `:userId` ;";
             return pool.myExecute(query,{
-                nameSpaceUserId,
                 nameSpaceRSC,
                 nameSpaceRSCs,
                 userId
             })
-            .then(subscribtions=> subscribtions.map(subscribtion => new Publisher(subscribtion.room)));        
+            .then(rooms=> rooms.map(room => new Publisher(room.id)));
         }
         
         static updateRecordStatus(userId,recordId,status){
-            const query = "UPDATE `:nameSpaceESCs` SET `relation`=:status,`updatedAt`=:now WHERE `:nameSpaceUserId` = :userId AND `recordId` = :recordId";
+            const query = "UPDATE `:nameSpaceESCs` SET `relation`=`:status`,`updatedAt`=`:now` WHERE `:user` = `:userId` AND `record` = `:recordId` ;";
             return pool.myExecute(query,{
                 nameSpaceESCs,
                 status,
                 now : moment(Date.now()).format('YYYY-MM-DD HH:mm:ss'),
-                nameSpaceUserId,
                 userId,
                 recordId
             });
         }
 
         static createRecordStatus(userId,recordId,status){
-            const query = "INSERT INTO `:nameSpaceESCs` (`id`,`relation`,`createdAt`,`updatedAt`,`recordId`,`:nameSpaceUserId`) VALUES (DEFAULT,:status,:now,:now,:recordId,:userId);";
+            const query = "INSERT INTO `:nameSpaceESCs` (`relation`,`createdAt`,`updatedAt`,`record`,`user`) VALUES (`:status`,`:now`,`:now`,`:recordId`,`:userId`);";
             return pool.myExecute(query,{
                 nameSpaceESCs,
-                nameSpaceUserId,
                 status,
                 recordId,
                 userId,
@@ -122,44 +113,36 @@ exports.get = nameSpace =>{
             });
         }
 
-        createRecord(record){
+        createRecord(data){
             const room = this.id;
-            const query = "INSERT INTO `:nameSpaceERCs` (`id`,`record`,`room`,`createdAt`,`updatedAt`) VALUES (DEFAULT,:record,:room,:now,:now);";
+            const query = "INSERT INTO `:nameSpaceERCs` (`id`,`data`,`room`,`createdAt`,`updatedAt`) VALUES (DEFAULT,`:data`,`:room`,`:now`,`:now`);";
             return pool.myExecute(query,{
                 nameSpaceERCs,
-                record : JSON.stringify(record),
+                data : JSON.stringify(data),
                 room,
                 now : moment(Date.now()).format('YYYY-MM-DD HH:mm:ss')
             });
         }
 
         getRecord(recordId){            
-            const query = 'SELECT `:nameSpaceERC`.`id`, `:nameSpaceERC`.`record`, `:nameSpaceERC`.`room`, `:nameSpaceERC`.`createdAt`, `:nameSpaceERC`.`updatedAt`, `:nameSpaceESCs`.`:nameSpaceUserId` AS `userId` FROM `:nameSpaceERCs` AS `:nameSpaceERC` INNER JOIN `:nameSpaceESCs` AS `:nameSpaceESCs` ON `:nameSpaceERC`.`id` = `:nameSpaceESCs`.`recordId` AND `:nameSpaceESCs`.`relation` = :relation WHERE `:nameSpaceERC`.`id` = :recordId;'
+            const query = 'SELECT `:nameSpaceERCs`.`id`, `:nameSpaceERCs`.`data`, `:nameSpaceERCs`.`room`, `:nameSpaceERCs`.`createdAt`, `:nameSpaceERCs`.`updatedAt`, `:nameSpaceERCs`.`author` FROM `:nameSpaceERCs` WHERE `:nameSpaceERCs`.`id` = :recordId LIMIT 1 ;'
             return pool.myExecute(query,{
-                relation : 'owner',
                 recordId,
-                nameSpaceERC,
-                nameSpaceERCs,
-                nameSpaceESCs,
-                nameSpaceUserId            
+                nameSpaceERCs            
             })
         }
 
-        getAllRecords(){
+        getRecordsByRoom(){
             const room = this.id;
-            const query = 'SELECT `:nameSpaceERC`.`id`, `:nameSpaceERC`.`record`, `:nameSpaceERC`.`room`, `:nameSpaceERC`.`createdAt`, `:nameSpaceERC`.`updatedAt`, `:nameSpaceESCs`.`:nameSpaceUserId` AS `userId` FROM `:nameSpaceERCs` AS `:nameSpaceERC` INNER JOIN `:nameSpaceESCs` AS `:nameSpaceESCs` ON `:nameSpaceERC`.`id` = `:nameSpaceESCs`.`recordId` AND `:nameSpaceESCs`.`relation` = :relation WHERE `:nameSpaceERC`.`room` = :room;'
+            const query = 'SELECT `:nameSpaceERCs`.`id`, `:nameSpaceERCs`.`data`, `:nameSpaceERCs`.`room`, `:nameSpaceERCs`.`createdAt`, `:nameSpaceERCs`.`updatedAt`, `:nameSpaceERCs`.`author` FROM `:nameSpaceERCs` WHERE `:nameSpaceERCs`.`room` = :room ;'
             return pool.myExecute(query,{
-                relation : 'owner',
                 room,
-                nameSpaceERC,
-                nameSpaceERCs,
-                nameSpaceESCs,
-                nameSpaceUserId                
+                nameSpaceERCs                
             })
         }
 
         updateRecord(record){
-            const query = "UPDATE `:nameSpaceERCs` SET `record`=:record,`updatedAt`=:now WHERE `recordId` = :recordId";
+            const query = "UPDATE `:nameSpaceERCs` SET `data`=:data,`updatedAt`=:now WHERE `id` = :recordId ;";
             return pool.myExecute(query,{
                 record : record.record,
                 recordId : record.id
@@ -167,22 +150,81 @@ exports.get = nameSpace =>{
         }
 
         deleteRecord(recordId){
-            const query = "DELETE FROM `:nameSpaceERCs` WHERE `id` = :recordId";
+            const query = "DELETE FROM `:nameSpaceERCs` WHERE `id` = :recordId;";
             return pool.myExecute(query,{
                 nameSpaceERCs,
                 recordId
             });
+        }
+
+        static createRoom(userId,data){
+            const query = "INSERT INTO `:nameSpaceRRCs`(`id`,`parent`,`admin`,`data`,`createdAt`,`updatedAt`) VALUES (DEFAULT,`:parent`,`:userId`,`:data`,`:now`,`:now`);";
+            return pool.myExecute(query,{
+                nameSpaceRRCs,
+                parent : null,
+                userId,
+                data,
+                now : moment(Date.now()).format('YYYY-MM-DD HH:mm:ss')
+            });
+        }
+
+        createRoom(userId,data){
+            const room = this.id;
+            const query = "INSERT INTO `:nameSpaceRRCs`(`id`,`parent`,`admin`,`data`,`createdAt`,`updatedAt`) VALUES (DEFAULT,`:parent`,`:userId`,`:data`,`:now`,`:now`);";
+            return pool.myExecute(query,{
+                nameSpaceRRCs,
+                parent : room,
+                userId,
+                data,
+                now : moment(Date.now()).format('YYYY-MM-DD HH:mm:ss')
+            });
+        }
+
+        deleteRoom(){
+            const room = this.id;
+            const query = "DELETE FROM `:nameSpaceRRCs` WHERE `id` = :room ;";
+            return pool.myExecute(query,{
+                nameSpaceRRCs,
+                room 
+            });
+        }
+
+        updateRoom(data){
+            const room = this.id;
+            const query = "UPDATE `:nameSpaceRRCs` SET `data`=:data,`updatedAt`=:now WHERE `id` = :room ;";
+            return pool.myExecute(query,{
+                nameSpaceRRCs,
+                room,
+                data
+            });
+        }
+
+        getData(){
+            const room = this.id;
+            const query = "SELECT * FROM `:nameSpaceRRCs` WHERE id = `:room`;";
+            return pool.myExecute(query,{
+                nameSpaceRRCs,
+                room
+            })
+        }
+
+        getSubscribersCount(){
+            const room = this.id;
+            const query = "SELECT COUNT(*) FROM `:nameSpaceRSCs` WHERE room = `:room` ;";
+            return pool.myExecute(query,{
+                nameSpaceRRCs,
+                room
+            })
         }
     }
     return Publisher;
 }
 
 exports.create = nameSpace => {
-    return pool.myExecute('DROP TABLE IF EXISTS `:nameSpaceUsers`;CREATE TABLE IF NOT EXISTS `:nameSpaceUsers` (`id` INTEGER NOT NULL auto_increment UNIQUE , `userName` VARCHAR(255) NOT NULL, `mail` VARCHAR(255), `createdAt` DATETIME NOT NULL, `updatedAt` DATETIME NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB;DROP TABLE IF EXISTS `:nameSpaceRSCs`;CREATE TABLE IF NOT EXISTS `:nameSpaceRSCs` (`id` INTEGER NOT NULL auto_increment , `room` VARCHAR(255), `createdAt` DATETIME NOT NULL, `updatedAt` DATETIME NOT NULL, `:nameSpaceUserId` INTEGER, PRIMARY KEY (`id`), FOREIGN KEY (`:nameSpaceUserId`) REFERENCES `:nameSpaceUsers` (`id`) ON DELETE SET NULL ON UPDATE CASCADE) ENGINE=InnoDB;DROP TABLE IF EXISTS `:nameSpaceERCs`;CREATE TABLE IF NOT EXISTS `:nameSpaceERCs` (`id` INTEGER NOT NULL auto_increment , `record` JSON NOT NULL, `room` VARCHAR(255) NOT NULL, `createdAt` DATETIME NOT NULL, `updatedAt` DATETIME NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB;DROP TABLE IF EXISTS `:nameSpaceESCs`;CREATE TABLE IF NOT EXISTS `:nameSpaceESCs` (`id` INTEGER NOT NULL auto_increment , `relation` VARCHAR(255), `createdAt` DATETIME NOT NULL,`updatedAt` DATETIME NOT NULL, `recordId` INTEGER, `:nameSpaceUserId` INTEGER, PRIMARY KEY (`id`), FOREIGN KEY (`recordId`) REFERENCES `:nameSpaceERCs` (`id`) ON DELETE SET NULL ON UPDATE CASCADE, FOREIGN KEY (`:nameSpaceUserId`) REFERENCES `:nameSpaceUsers` (`id`) ON DELETE SET NULL ON UPDATE CASCADE) ENGINE=InnoDB;CREATE UNIQUE INDEX dummy ON :nameSpaceESCs(recordId,:nameSpaceUserId , relation);CREATE UNIQUE INDEX anotherDummy ON :nameSpaceRSCs(room,:nameSpaceUserId);',{
+    return pool.myExecute('DROP TABLE IF EXISTS `:nameSpaceUsers`; CREATE TABLE IF NOT EXISTS `:nameSpaceUsers` (`id` INTEGER NOT NULL auto_increment UNIQUE , `userName` VARCHAR(255) NOT NULL, `mail` VARCHAR(255) UNIQUE, `createdAt` DATETIME NOT NULL, `updatedAt` DATETIME NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB;DROP TABLE IF EXISTS `:nameSpaceRRCs`; CREATE TABLE IF NOT EXISTS `:nameSpaceRRCs` (`id` INTEGER NOT NULL auto_increment UNIQUE , `parent` INTEGER, `data` JSON,`admin` INTEGER NOT NULL, `createdAt` DATETIME NOT NULL, `updatedAt` DATETIME NOT NULL, PRIMARY KEY (`id`), FOREIGN KEY (`admin`) REFERENCES `:nameSpaceUsers` ON DELETE CASCADE ON UPDATE CASCADE) ENGINE=InnoDB;DROP TABLE IF EXISTS `:nameSpaceRSCs`; CREATE TABLE IF NOT EXISTS `:nameSpaceRSCs` (`room` INTEGER NOT NULL, `user` INTEGER NOT NULL, `accessLevel` VARCHAR(255), `createdAt` DATETIME NOT NULL, `updatedAt` DATETIME NOT NULL, PRIMARY KEY (`room`,`user`), FOREIGN KEY (`room`) REFERENCES `:nameSpaceRRCs`(`id`) ON DELETE CASCADE ON UPDATE CASCADE, FOREIGN KEY (`user`) REFERENCES `:nameSpaceUsers` (`id`) ON DELETE CASCADE ON UPDATE CASCADE) ENGINE=InnoDB;DROP TABLE IF EXISTS `:nameSpaceERCs`; CREATE TABLE IF NOT EXISTS `:nameSpaceERCs` (`id` INTEGER NOT NULL auto_increment UNIQUE , `data` JSON NOT NULL, `room` INTEGER NOT NULL,`author` INTEGER NOT NULL, `createdAt` DATETIME NOT NULL, `updatedAt` DATETIME NOT NULL, PRIMARY KEY (`id`), FOREIGN KEY (`room`) REFERENCES `:nameSpaceRRCs` (`id`) ON DELETE CASCADE ON UPDATE CASCADE, FOREIGN KEY (`author`) REFERENCES `:nameSpaceUsers` ON DELETE CASCADE ON UPDATE CASCADE) ENGINE=InnoDB;DROP TABLE IF EXISTS `:nameSpaceESCs`;CREATE TABLE IF NOT EXISTS `:nameSpaceESCs` (`relation` VARCHAR(255), `record` INTEGER NOT NULL, `user` INTEGER NOT NULL, `createdAt` DATETIME NOT NULL, `updatedAt` DATETIME NOT NULL, PRIMARY KEY (`record`,`user`), FOREIGN KEY (`record`) REFERENCES `:nameSpaceERCs` (`id`), FOREIGN KEY (`user`) REFERENCES `:nameSpaceUsers` (`id`)) ENGINE=InnoDB;',{
         nameSpaceERCs   : `${nameSpace}ERCs`  ,
         nameSpaceESCs   : `${nameSpace}ESCs`  ,
         nameSpaceRSCs   : `${nameSpace}RSCs`  ,
-        nameSpaceUserId : `${nameSpace}UserId`,
         nameSpaceUsers  : `${nameSpace}Users`
     });
 }
