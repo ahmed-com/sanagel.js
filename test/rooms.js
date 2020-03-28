3b20650e729ad74e98f0ba40f64075545efb8483
ahmed-com <52245980+ahmed-com@users.noreply.github.com>
2020-03-28 03:48:36 +0200

    working on merging

--- test/rooms.js
+++
@@ -1,3 +0,0 @@
-const expect = require('chai').expect;
-const Sequelize = require('sequelize');
-const db = new Sequelize('testing','root',process.env.DBPASSWORD,{dialect: 'mysql', host : 'localhost'});
