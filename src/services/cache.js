119f8ab6d359dac1367d1b15b3c51c08c7471932
ahmed-com <52245980+ahmed-com@users.noreply.github.com>
2020-06-09 08:55:13 +0200

    decided to omit the caching logic untill I fix it

--- src/services/cache.js
+++ src/services/cache.js
@@ -1,63 +1,63 @@
 const pool = require('../utils/db');
-const client = require('../utils/redis').getClient();
+// const client = require('../utils/redis').getClient();
 
-const hget = (key,field)=>{
-    return new Promise((resolve,reject)=>{
-        client.hget(key,field,(err,value)=>{
-            err ? reject(err) : resolve(value);
-        });
-    })
-}
+// const hget = (key,field)=>{
+//     return new Promise((resolve,reject)=>{
+//         client.hget(key,field,(err,value)=>{
+//             err ? reject(err) : resolve(value);
+//         });
+//     })
+// }
 
-const hset = client.hset.bind(client);
+// const hset = client.hset.bind(client);
 
-const del = client.del;
+// const del = client.del;
 
 const roomRead = async (nameSpace,room,query,data)=>{
-    const key = JSON.stringify({nameSpace,room});
-    const field = JSON.stringify({query,data});
-    let cachedResult = await hget(key,field);
-    if(cachedResult){
-        return JSON.parse(cachedResult);
-    }
+    // const key = JSON.stringify({nameSpace,room});
+    // const field = JSON.stringify({query,data});
+    // let cachedResult = await hget(key,field);
+    // if(cachedResult){
+    //     return JSON.parse(cachedResult);
+    // }
     const result = await pool.myExecute(query,data);
-    setImmediate(()=>{
-        cachedResult = JSON.stringify(result);
-        hset(key,field,cachedResult);
-    });
+    // setImmediate(()=>{
+    //     cachedResult = JSON.stringify(result);
+    //     hset(key,field,cachedResult);
+    // });
     return result;
 }
 
 const roomWrite = async (nameSpace,room,query,data)=>{
-    setImmediate(()=>{
-        const key = JSON.stringify({nameSpace,room});
-        client.exists(key,bool=>{
-            if (bool) del(key);
-        })
-    });
+    // setImmediate(()=>{
+    //     const key = JSON.stringify({nameSpace,room});
+    //     client.exists(key,bool=>{
+    //         if (bool) del(key);
+    //     })
+    // });
     return pool.myExecute(query,data)
 }
 
 const userRead = async (nameSpace,user,query,data)=>{
-    const key = JSON.stringify({nameSpace,user});
-    const field = JSON.stringify({query,data});
-    let cachedResult = await hget(key,field);
-    if(cachedResult){
-        return JSON.parse(cachedResult);
-    }
+    // const key = JSON.stringify({nameSpace,user});
+    // const field = JSON.stringify({query,data});
+    // let cachedResult = await hget(key,field);
+    // if(cachedResult){
+    //     return JSON.parse(cachedResult);
+    // }
     const result = await pool.myExecute(query,data);
-    setImmediate(()=>{
-        cachedResult = JSON.stringify(result);
-        hset(key,field,cachedResult);
-    });
+    // setImmediate(()=>{
+    //     cachedResult = JSON.stringify(result);
+    //     hset(key,field,cachedResult);
+    // });
     return result;
 }
 
 const userWrite = async (nameSpace,user,query,data)=>{
-    setImmediate(()=>{
-        const key = JSON.stringify({nameSpace,user});
-        del(key);
-    });
+    // setImmediate(()=>{
+    //     const key = JSON.stringify({nameSpace,user});
+    //     del(key);
+    // });
     return pool.myExecute(query,data);
 }
 
